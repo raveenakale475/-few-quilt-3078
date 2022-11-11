24c11670e01c101478a2e5df91da01262fc247ae
raveena <raveenar.kale@gmail.com>
2022-11-11 14:55:20 +0530

    home added

--- lyst/src/App.js
+++ lyst/src/App.js
@@ -1,17 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import Carousel from "./Components/Carousel";
-import Middle from "./Components/Middle";
-import { slideOne, firstSlide, secondSlide, slideTwo } from "./Components/data";
+import Home from "./Components/Home/Home";
+// import Navbar from "./Components/Navbar";
+// import Carousel from "./Components/Carousel";
+// import Middle from "./Components/Middle";
+// import { slideOne, firstSlide, secondSlide, slideTwo } from "./Components/data";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
+      {/* <Navbar />
       <Carousel />
       {slideOne()}
-      {slideTwo()}
+      {slideTwo()} */}
     </div>
   );
 }
